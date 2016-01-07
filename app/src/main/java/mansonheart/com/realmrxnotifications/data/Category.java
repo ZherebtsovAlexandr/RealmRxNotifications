d802c6cccda395abbd1b54fe3289847395550b6e
mansonheart <zherebtsovalexandr@gmail.com>
2016-01-07 22:29:58 +0300

    Realm models

---
+++ app/src/main/java/mansonheart/com/realmrxnotifications/data/Category.java
@@ -0,0 +1,32 @@
+package mansonheart.com.realmrxnotifications.data;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+/**
+ * Created by Zherebtsov Alexandr on 07.01.2016.
+ */
+public class Category extends RealmObject {
+
+    @PrimaryKey
+    private int categoryId;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+}
+
---
+++ app/src/main/java/mansonheart/com/realmrxnotifications/data/Event.java
@@ -0,0 +1,39 @@
+package mansonheart.com.realmrxnotifications.data;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+/**
+ * Created by Zherebtsov Alexandr on 07.01.2016.
+ */
+public class Event extends RealmObject {
+
+    @PrimaryKey
+    private int eventId;
+    private String title;
+    private Category category;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(int eventId) {
+        this.eventId = eventId;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
