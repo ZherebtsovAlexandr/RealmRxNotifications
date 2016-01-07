cddd81073a5cf23a659766f27a3c12755f79d20a
mansonheart <zherebtsovalexandr@gmail.com>
2016-01-07 20:15:38 +0300

    Add Realm access layer

---
+++ database/src/main/java/mansonheart/com/database/RealmAccessLayer.java
@@ -0,0 +1,19 @@
+package mansonheart.com.database;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import io.realm.RealmQuery;
+import rx.Observable;
+import rx.functions.Action0;
+import rx.functions.Func1;
+
+/**
+ * Created by Zherebtsov Alexandr on 07.01.2016.
+ */
+public interface RealmAccessLayer {
+    <T> Observable<T> get(Class clazz, Func1<RealmQuery, RealmQuery> predicate);
+    void storeObject(Class clazz, JSONObject jsonObject);
+    void storeObjects(Class clazz, JSONArray jsonArray);
+    <T> Observable<T> update(Class clazz, Action0 action);
+}
