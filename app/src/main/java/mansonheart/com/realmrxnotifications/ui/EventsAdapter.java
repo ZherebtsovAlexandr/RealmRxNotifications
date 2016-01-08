6490d3eb4e7a4d474ecf0ac683ab52127601c536
mansonheart <zherebtsovalexandr@gmail.com>
2016-01-08 20:21:39 +0300

    UI

---
+++ app/src/main/java/mansonheart/com/realmrxnotifications/ui/EventsAdapter.java
@@ -0,0 +1,74 @@
+package mansonheart.com.realmrxnotifications.ui;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.Collections;
+import java.util.List;
+
+import mansonheart.com.realmrxnotifications.R;
+import mansonheart.com.realmrxnotifications.data.Event;
+import rx.functions.Action1;
+
+/**
+ * Created by Zherebtsov Alexandr on 08.01.2016.
+ */
+public final class EventsAdapter extends BaseAdapter implements Action1<List<Event>> {
+    private final LayoutInflater inflater;
+
+    private List<Event> items = Collections.emptyList();
+
+    public EventsAdapter(Context context) {
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public void call(List<Event> items) {
+        this.items = items;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+    @Override
+    public Event getItem(int position) {
+        return items.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return getItem(position).getEventId();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.event_item, parent, false);
+            ViewHolder holder = new ViewHolder();
+            holder.title = (TextView) convertView.findViewById(R.id.title);
+            holder.category = (TextView) convertView.findViewById(R.id.category);
+            convertView.setTag(holder);
+        }
+
+        ViewHolder holder = (ViewHolder) convertView.getTag();
+        Event item = getItem(position);
+        CharSequence title = item.getTitle();
+        CharSequence category = item.getCategory() != null
+                ? item.getCategory().getName() : "";
+        holder.title.setText(title);
+        holder.category.setText(category);
+        return convertView;
+    }
+
+    public static class ViewHolder {
+        TextView title;
+        TextView category;
+    }
+}
